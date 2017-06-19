e67d9d368638b4df8c65eea9ddde52fc04557fec
dgketchum <dgketchum@gmail.com>
2017-06-19 12:16:55 -0600

    adding DEM class

--- metio/misc.py
+++ metio/misc.py
@@ -1,5 +1,10 @@
-# Adapted from work by Jared Oyler, https://github.com/jaredwo
+from future.standard_library import hooks
+
+with hooks():
+    from urllib.parse import urlunparse
+
 import os
+from xarray import open_dataset
 
 
 class BBox(object):
@@ -12,7 +17,6 @@ class BBox(object):
 
     def __init__(self, west_lon=-126.0, south_lat=22.0, east_lon=-64.0,
                  north_lat=53.0):
-
         self.west = west_lon
         self.south = south_lat
         self.east = east_lon
@@ -27,6 +31,38 @@ class BBox(object):
         df = df[mask_bnds].copy()
 
         return df
+
+
+class Dem(object):
+    """ Digital Elevation Model and Dertivatives
+    
+    :param BBox, bounding box
+    """
+
+    def __init__(self, bbox=None):
+        self.bbox = bbox
+
+    def thredds_dem(self, elevation=None, slope=None, aspect=None):
+        service = 'thredds.northwestknowledge.net:8080'
+        scheme = 'http'
+
+        url = urlunparse([scheme, service,
+                          '/thredds/dodsC/MET/elev/metdata_elevationdata.nc',
+                          '', '', ''])
+
+        xray = open_dataset(url)
+
+        subset = xray.loc[dict(lat=slice(self.bbox.north, self.bbox.south),
+                               lon=slice(self.bbox.west, self.bbox.east))]
+
+        if aspect:
+            pass
+
+        if slope:
+            pass
+
+        if elevation:
+            return subset
 
 
 if __name__ == '__main__':
