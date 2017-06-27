c47151bb26c5cb8d6d851be96a07e488457ca818
dgketchum <dgketchum@gmail.com>
2017-06-27 16:45:40 -0600

    aspect and slope working

--- tests/test_dem.py
+++ tests/test_dem.py
@@ -46,11 +46,11 @@ class DemTestCase(unittest.TestCase):
         dem = MapzenDem(zoom=10, bounds=bb, target_profile=profile, clip_object=polygon,
                         api_key=self.api_key)
 
-        arr = dem.dem(out_file='/data01/images/sandbox/merged_dem.tif')
+        arr = dem.terrain(out_file='/data01/images/sandbox/merged_dem.tif')
 
         self.assertEqual(arr.shape, (1, 7429, 8163))
 
-    def test_terrain(self):
+    def test_slope(self):
         home = os.path.expanduser('~')
         tif_dir = os.path.join(home, 'images', 'LT5', 'image_test', 'full_image')
         tif = os.path.join(tif_dir, 'LT05_L1TP_040028_20060706_20160909_01_T1_B5.TIF')
@@ -63,11 +63,26 @@ class DemTestCase(unittest.TestCase):
         dem = MapzenDem(zoom=10, bounds=bb, target_profile=profile, clip_object=polygon,
                         api_key=self.api_key)
 
-        slope = dem.get_slope(out_file='/data01/images/sandbox/slope.tif')
-        aspect = dem.get_aspect(out_file='/data01/images/sandbox/aspect.tif', mode='degrees')
+        slope = dem.terrain(attribute='slope', out_file='/data01/images/sandbox/slope.tif')
 
         self.assertEqual(slope.shape, (1, 7429, 8163))
-        self.assertEqual(aspect.shape, (1, 7429, 8163))
+
+    def test_aspect(self):
+        home = os.path.expanduser('~')
+        tif_dir = os.path.join(home, 'images', 'LT5', 'image_test', 'full_image')
+        tif = os.path.join(tif_dir, 'LT05_L1TP_040028_20060706_20160909_01_T1_B5.TIF')
+
+        l5 = Landsat5(tif_dir)
+        bb = RasterBounds(tif)
+        polygon = l5.get_tile_geometry()
+        profile = l5.rasterio_geometry
+
+        dem = MapzenDem(zoom=10, bounds=bb, target_profile=profile, clip_object=polygon,
+                        api_key=self.api_key)
+
+        aspect = dem.terrain(attribute='aspect', out_file='/data01/images/sandbox/aspect.tif')
+
+        self.assertEqual(aspect.shape, (7429, 8163))
 
     def test_gibs(self):
         self.assertIsInstance(self.dem, Dem)
