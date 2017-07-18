0e21b88b8bd7bd4a97671a0eabe23168c2db11d6
dgketchum <dgketchum@gmail.com>
2017-07-18 12:37:04 -0600

    build out topowx, put common methods in Thredds parent

--- tests/test_metio.py
+++ tests/test_metio.py
@@ -19,7 +19,7 @@ from datetime import datetime
 from xarray import Dataset
 
 from bounds.bounds import GeoBounds, RasterBounds
-from metio.thredds import GridMet
+from metio.thredds import GridMet, TopoWX
 from sat_image.image import Landsat5
 
 
@@ -28,7 +28,7 @@ class TestGridMet(unittest.TestCase):
         self.bbox = GeoBounds(west_lon=-116.4, east_lon=-103.0,
                               south_lat=44.3, north_lat=49.1)
 
-        self.vars = ['pr', 'pet', 'not_a_var']
+        self.vars = ['pr', 'pet', 'not_a_var', 'elev', 'srad']
         self.test_url_str = 'http://thredds.northwestknowledge.net:8080/thredds/ncss/MET/pet/pet_2011.nc?' \
                             'var=potential_evapotranspiration&north=49.1&west=-116.4&east=-103.0&south=44.3&' \
                             '&horizStride=1&time_start=2011-01-01T00%3A00%3A00Z&' \
@@ -71,6 +71,26 @@ class TestGridMet(unittest.TestCase):
                           target_profile=l5.profile, clip_feature=polygon)
         gridmet.get_data_subset(grid_conform=True)
         self.assertEqual(gridmet.shape, (1, 1, 1))
+
+
+class TestTopoWX(unittest.TestCase):
+    def setUp(self):
+        self.start = datetime(2011, 4, 1)
+        self.date = datetime(2011, 4, 1)
+        self.end = datetime(2011, 10, 31)
+
+        self.dir_name_LT5 = 'tests/data/image_test/lt5_image'
+
+    def test_conforming_array(self):
+        home = os.path.expanduser('~')
+        tif_dir = os.path.join(home, 'images', 'LT5', 'image_test', 'full_image')
+        l5 = Landsat5(tif_dir)
+        polygon = l5.get_tile_geometry()
+        bounds = RasterBounds(affine_transform=l5.transform, profile=l5.profile)
+        topowx = TopoWX(date=self.date, bbox=bounds, target_profile=l5.profile,
+                        clip_feature=polygon)
+        topowx.get_data_subset(grid_conform=True)
+        self.assertEqual(topowx.shape, (1, 1, 1))
 
 if __name__ == '__main__':
     unittest.main()
