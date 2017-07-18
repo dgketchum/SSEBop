60bb956631a5294ac26f324aae01c0d310e217ad
dgketchum <dgketchum@gmail.com>
2017-07-18 12:37:04 -0600

    build out topowx, put common methods in Thredds parent

--- metio/thredds.py
+++ metio/thredds.py
@@ -51,73 +51,187 @@ class Thredds(object):
         self.target_profile = target_profile
         self.bbox = bounds
 
+    def conform(self, subset, var):
+        self._project(subset)
+        result = self._resample(var)
+        return result
+
+    def _project(self, subset):
+        proj_path = os.path.join(self.temp_dir, 'tiled_proj.tif')
+        setattr(self, 'projection', proj_path)
+
+        profile = copy.deepcopy(self.target_profile)
+        profile['dtype'] = float32
+        bb = self.bbox.as_tuple()
+        bounds = (bb[0], bb[1],
+                  bb[2], bb[3])
+        dst_affine, dst_width, dst_height = cdt(CRS({'init': 'epsg:4326'}),
+                                                profile['crs'],
+                                                subset.shape[1],
+                                                subset.shape[2],
+                                                *bounds)
+
+        profile.update({'crs': profile['crs'],
+                        'transform': dst_affine,
+                        'width': dst_width,
+                        'height': dst_height})
+
+        with rasopen(proj_path, 'w', **profile) as dst:
+            dst.write(subset)
+
+    def _resample(self, var):
+        home = os.path.expanduser('~')
+        temp_path = os.path.join(home, 'images', 'sandbox', 'resample_met_{}.tif'.format(var))
+        # temp_path = os.path.join(self.temp_dir, 'resample.tif')
+
+        with rasopen(self.projection, 'r') as src:
+            array = src.read(1)
+            profile = src.profile
+            res = src.res
+            target_res = self.target_profile['transform'].a
+            res_coeff = res[0] / target_res
+
+            new_array = empty(shape=(1, round(array.shape[0] * res_coeff - 2),
+                                     round(array.shape[1] * res_coeff)), dtype=float32)
+            aff = src.transform
+            new_affine = Affine(aff.a / res_coeff, aff.b, aff.c, aff.d, aff.e / res_coeff, aff.f)
+
+            profile['transform'] = self.target_profile['transform']
+            profile['width'] = self.target_profile['width']
+            profile['height'] = self.target_profile['height']
+            profile['dtype'] = new_array.dtype
+
+            with rasopen(temp_path, 'w', **profile) as dst:
+                reproject(array, new_array, src_transform=aff,
+                          dst_transform=new_affine, src_crs=src.crs,
+                          dst_crs=src.crs, resampling=Resampling.cubic)
+
+                dst.write(new_array)
+
+            return new_array
+
+            # add no-data values TODO
+
+    def _date_index(self):
+        date_ind = date_range(self.start, self.end, freq='d')
+
+        return date_ind
+
+    def _dtime_to_xldate(self):
+        s_sup, e_sup = self.start.timetuple(), self.end.timetuple()
+        s_tup, e_tup = (s_sup[0], s_sup[1], s_sup[2]), (e_sup[0], e_sup[1], e_sup[2])
+        sxl, exl = xldate_from_date_tuple(s_tup, 0), xldate_from_date_tuple(e_tup, 0)
+        return sxl, exl
+
 
 class TopoWX(Thredds):
-    """ TopoWX Surface Temperature, return as numpy array in daily stack unless modified.
+    # """ TopoWX Surface Temperature, return as numpy array in daily stack unless modified.
+    #
+    # Available variables: [ 'tmmn', 'tmmx']
+    #
+    # ----------
+    # Observation elements to access. Currently available elements:
+    # - 'tmmn' : daily minimum air temperature [K]
+    # - 'tmmx' : daily maximum air temperature [K]
+    #
+    # :param start: datetime object start of period of data
+    # :param end: datetime object end of period of data
+    # :param variables: List  of available variables. At lease one.
+    # :param date: single-day datetime date object
+    # :param bounds: metio.misc.BBox object representing spatial bounds, default to conterminous US
+    # :return: numpy.ndarray
+    #
+    # """
 
-    Available variables: [ 'tmmn', 'tmmx']
-    
-    ----------
-    Observation elements to access. Currently available elements:
-    - 'tmmn' : daily minimum air temperature [K]
-    - 'tmmx' : daily maximum air temperature [K]
+    def __init__(self, **kwargs):
+        Thredds.__init__(self)
 
-    :param start: datetime object start of period of data
-    :param end: datetime object end of period of data
-    :param variables: List  of available variables. At lease one. 
-    :param date: single-day datetime date object
-    :param bounds: metio.misc.BBox object representing spatial bounds, default to conterminous US
-    :return: numpy.ndarray
-    
-    """
+        for key, val in kwargs.items():
+            setattr(self, key, val)
 
-    def __init__(self):
-        Thredds.__init__(self, start=None, end=None, date=None, bounds=None)
+        self.variables = ['tmin', 'tmax']
 
         if self.start:
-            pass
+            self.year = self.start.year
+        else:
+            self.year = self.date.year
+
+    def get_data_subset(self, grid_conform=False):
+
+        for var in self.variables:
+
+            url = self._build_url(var)
+            xray = open_dataset(url)
+
+            start_xl, end_xl = self._dtime_to_xldate()
+
+            subset = xray.loc[dict(day=slice(start_xl, end_xl),
+                                   lat=slice(self.bbox.north, self.bbox.south),
+                                   lon=slice(self.bbox.west, self.bbox.east))]
+            subset.rename({'day': 'time'}, inplace=True)
+            date_ind = self._date_index()
+            subset['time'] = date_ind
+            setattr(self, 'width', subset.dims['lon'])
+            setattr(self, 'height', subset.dims['lat'])
+
+            if not grid_conform:
+                setattr(self, var, subset.values)
+            else:
+                conformed_array = self.conform(subset, var)
+                setattr(self, var, conformed_array)
+
+        return None
+
+    def _build_url(self, var):
+
+        # ParseResult('scheme', 'netloc', 'path', 'params', 'query', 'fragment')
+        url = urlunparse([self.scheme, self.service,
+                          '/thredds/dodsC/topowx.nc'.format(var),
+                          '', '', ''])
+
+        return url
 
 
 class GridMet(Thredds):
-    """ U of I Gridmet, return as numpy array per met variable in daily stack unless modified.
-    
-    Available variables: ['bi', 'elev', 'erc', 'fm100', fm1000', 'pdsi', 'pet', 'pr', 'rmax', 'rmin', 'sph', 'srad',
-                          'th', 'tmmn', 'tmmx', 'vs']
-        ----------
-        Observation elements to access. Currently available elements:
-        - 'bi' : burning index [-]
-        - 'elev' : elevation above sea level [m]
-        - 'erc' : energy release component [-]
-        - 'fm100' : 100-hour dead fuel moisture [%]
-        - 'fm1000' : 1000-hour dead fuel moisture [%]
-        - 'pdsi' : Palmer Drough Severity Index [-]
-        - 'pet' : daily reference potential evapotranspiration [mm]
-        - 'pr' : daily accumulated precipitation [mm]
-        - 'rmax' : daily maximum relative humidity [%]
-        - 'rmin' : daily minimum relative humidity [%]
-        - 'sph' : daily mean specific humidity [kg/kg]
-        - 'srad' : daily maximum relative humidity [%]
-        - 'prcp' : daily total precipitation [mm]
-        - 'srad' : daily mean downward shortwave radiation at surface [W m-2]
-        - 'th' : daily mean wind direction clockwise from North [degrees] 
-        - 'tmmn' : daily minimum air temperature [K]
-        - 'tmmx' : daily maximum air temperature [K]
-        - 'vs' : daily mean wind speed [m -s]
-            
-    :param start: datetime object start of period of data
-    :param end: datetime object end of period of data
-    :param variables: List  of available variables. At lease one. 
-    :param date: single-day datetime date object
-    :param bbox: metio.misc.BBox object representing spatial bounds, default to conterminous US
-    :return: numpy.ndarray
-    
-    Must have either start and end, or date.
-    Must have at least one valid variable. Invalid variables will be excluded gracefully.
-
-    note: NetCDF dates are in xl '1900' format, i.e., number of days since 1899-12-31 23:59
-          xlrd.xldate handles this for the time being
-          
-    """
+    # """ U of I Gridmet, return as numpy array per met variable in daily stack unless modified.
+    #
+    # Available variables: ['bi', 'elev', 'erc', 'fm100', fm1000', 'pdsi', 'pet', 'pr', 'rmax', 'rmin', 'sph', 'srad',
+    #                       'th', 'tmmn', 'tmmx', 'vs']
+    #     ----------
+    #     Observation elements to access. Currently available elements:
+    #     - 'bi' : burning index [-]
+    #     - 'elev' : elevation above sea level [m]
+    #     - 'erc' : energy release component [-]
+    #     - 'fm100' : 100-hour dead fuel moisture [%]
+    #     - 'fm1000' : 1000-hour dead fuel moisture [%]
+    #     - 'pdsi' : Palmer Drough Severity Index [-]
+    #     - 'pet' : daily reference potential evapotranspiration [mm]
+    #     - 'pr' : daily accumulated precipitation [mm]
+    #     - 'rmax' : daily maximum relative humidity [%]
+    #     - 'rmin' : daily minimum relative humidity [%]
+    #     - 'sph' : daily mean specific humidity [kg/kg]
+    #     - 'srad' : daily maximum relative humidity [%]
+    #     - 'prcp' : daily total precipitation [mm]
+    #     - 'srad' : daily mean downward shortwave radiation at surface [W m-2]
+    #     - 'th' : daily mean wind direction clockwise from North [degrees]
+    #     - 'tmmn' : daily minimum air temperature [K]
+    #     - 'tmmx' : daily maximum air temperature [K]
+    #     - 'vs' : daily mean wind speed [m -s]
+    #
+    # :param start: datetime object start of period of data
+    # :param end: datetime object end of period of data
+    # :param variables: List  of available variables. At lease one.
+    # :param date: single-day datetime date object
+    # :param bbox: metio.misc.BBox object representing spatial bounds, default to conterminous US
+    # :return: numpy.ndarray
+    #
+    # Must have either start and end, or date.
+    # Must have at least one valid variable. Invalid variables will be excluded gracefully.
+    #
+    # note: NetCDF dates are in xl '1900' format, i.e., number of days since 1899-12-31 23:59
+    #       xlrd.xldate handles this for the time being
+    #
+    # """
 
     def __init__(self, variables, **kwargs):
         Thredds.__init__(self)
@@ -172,109 +286,48 @@ class GridMet(Thredds):
             url = self._build_url(var)
             xray = open_dataset(url)
 
-            start_xl, end_xl = self._dtime_to_xldate()
+            if var != 'elev':
+                start_xl, end_xl = self._dtime_to_xldate()
 
-            subset = xray.loc[dict(day=slice(start_xl, end_xl),
-                                   lat=slice(self.bbox.north, self.bbox.south),
-                                   lon=slice(self.bbox.west, self.bbox.east))]
+                subset = xray.loc[dict(day=slice(start_xl, end_xl),
+                                       lat=slice(self.bbox.north, self.bbox.south),
+                                       lon=slice(self.bbox.west, self.bbox.east))]
+                subset.rename({'day': 'time'}, inplace=True)
+                date_ind = self._date_index()
+                subset['time'] = date_ind
+                setattr(self, 'width', subset.dims['lon'])
+                setattr(self, 'height', subset.dims['lat'])
+                if not grid_conform:
+                    setattr(self, var, subset)
+                else:
+                    arr = subset[self.kwords[var]].values
+                    conformed_array = self.conform(arr, var)
+                    setattr(self, var, conformed_array)
 
-            subset.rename({'day': 'time'}, inplace=True)
-            date_ind = self._date_index()
-            subset['time'] = date_ind
-            setattr(self, 'width', subset.dims['lon'])
-            setattr(self, 'height', subset.dims['lat'])
-            if not grid_conform:
-                setattr(self, var, subset)
             else:
-                array = subset[self.kwords[var]].values
-                conformed_array = self.conform(array, var)
+
+                setattr(self, 'width', subset.dims['lon'])
+                setattr(self, 'height', subset.dims['lat'])
+                subset = xray.loc[dict(lat=slice(self.bbox.north, self.bbox.south),
+                                       lon=slice(self.bbox.west, self.bbox.east))]
+                arr = subset.elevation.values
+                conformed_array = self.conform(arr, var)
                 setattr(self, var, conformed_array)
 
         return None
 
-    def conform(self, subset, var):
-        self.project(subset, var)
-        result = self.resample()
-        return result
-
-    def project(self, subset, var):
-
-        # home = os.path.expanduser('~')
-        # temp_path = os.path.join(home, 'images', 'sandbox', 'project_met_{}.tif'.format(var))
-
-        proj_path = os.path.join(self.temp_dir, 'tiled_proj.tif')
-        setattr(self, 'projection', proj_path)
-
-        profile = copy.deepcopy(self.target_profile)
-        profile['dtype'] = float32
-        bb = self.bbox.as_tuple()
-        bounds = (bb[0], bb[1],
-                  bb[2], bb[3])
-        dst_affine, dst_width, dst_height = cdt(CRS({'init': 'epsg:4326'}),
-                                                profile['crs'],
-                                                subset.shape[1],
-                                                subset.shape[2],
-                                                *bounds)
-
-        profile.update({'crs': profile['crs'],
-                        'transform': dst_affine,
-                        'width': dst_width,
-                        'height': dst_height})
-
-        with rasopen(proj_path, 'w', **profile) as dst:
-            dst.write(subset)
-
-    def resample(self):
-
-        temp_path = os.path.join(self.temp_dir, 'resample.tif')
-
-        with rasopen(self.projection, 'r') as src:
-            array = src.read(1)
-            profile = src.profile
-            res = src.res
-            target_res = self.target_profile['transform'].a
-            res_coeff = res[0] / target_res
-
-            new_array = empty(shape=(1, round(array.shape[0] * res_coeff - 2),
-                                     round(array.shape[1] * res_coeff)), dtype=float32)
-            aff = src.transform
-            new_affine = Affine(aff.a / res_coeff, aff.b, aff.c, aff.d, aff.e / res_coeff, aff.f)
-
-            profile['transform'] = self.target_profile['transform']
-            profile['width'] = self.target_profile['width']
-            profile['height'] = self.target_profile['height']
-            profile['dtype'] = new_array.dtype
-
-            with rasopen(temp_path, 'w', **profile) as dst:
-                reproject(array, new_array, src_transform=aff,
-                          dst_transform=new_affine, src_crs=src.crs,
-                          dst_crs=src.crs, resampling=Resampling.cubic)
-
-                dst.write(new_array)
-
-            return new_array
-
-            # add no-data values TODO
-
     def _build_url(self, var):
 
         # ParseResult('scheme', 'netloc', 'path', 'params', 'query', 'fragment')
-        url = urlunparse([self.scheme, self.service,
-                          '/thredds/dodsC/MET/{0}/{0}_{1}.nc'.format(var, self.year),
-                          '', '', ''])
+        if var == 'elev':
+            url = urlunparse([self.scheme, self.service,
+                              '/thredds/dodsC/MET/{0}/metdata_elevationdata.nc'.format(var),
+                              '', '', ''])
+        else:
+            url = urlunparse([self.scheme, self.service,
+                              '/thredds/dodsC/MET/{0}/{0}_{1}.nc'.format(var, self.year),
+                              '', '', ''])
 
         return url
-
-    def _dtime_to_xldate(self):
-        s_sup, e_sup = self.start.timetuple(), self.end.timetuple()
-        s_tup, e_tup = (s_sup[0], s_sup[1], s_sup[2]), (e_sup[0], e_sup[1], e_sup[2])
-        sxl, exl = xldate_from_date_tuple(s_tup, 0), xldate_from_date_tuple(e_tup, 0)
-        return sxl, exl
-
-    def _date_index(self):
-
-        date_ind = date_range(self.start, self.end, freq='d')
-
-        return date_ind
 
 # ========================= EOF ====================================================================
