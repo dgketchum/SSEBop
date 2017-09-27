609a66ba35240948ba19f832747fd0432133bec1
dgketchum <dgketchum@gmail.com>
2017-09-27 08:18:03 -0600

    adding point extract utility to image lst tests

--- tests/test_image_lst.py
+++ tests/test_image_lst.py
@@ -15,14 +15,17 @@
 # ===============================================================================
 
 import unittest
+from fiona import open as fopen
+from rasterio import open as rasopen
 
-from sat_image.image import Landsat8, Landsat5, Landsat7
+from sat_image.image import Landsat8, Landsat5, Landsat7, LandsatImage
 
 
 class TestImageLST5(unittest.TestCase):
     def setUp(self):
         self.dir_name_LT5 = 'tests/data/ssebop_test/lt5/041_025/2000/LT50410252000194AAA01'
         self.l5 = Landsat5(self.dir_name_LT5)
+
 
     def test_something(self):
         self.assertEqual(True, False)
@@ -45,6 +48,26 @@ class TestImageLST8(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, False)
 
+
+def raster_point_row_col(raster, points):
+    pt_data = {}
+
+    with fopen(points, 'r') as src:
+        for feature in src:
+            pt_data[feature['id']] = feature
+
+    if isinstance(raster, LandsatImage):
+        a = raster.rasterio_geometry['affine']
+    if isinstance(raster, str):
+        with rasopen(raster, 'r') as src:
+            a = src.affine
+
+    for key, val in pt_data.items():
+        x, y = val['geometry']['coordinates'][0], val['geometry']['coordinates'][1]
+        col, row = ~a * (x, y)
+        val['index'] = row, col
+
+    return pt_data
 
 if __name__ == '__main__':
     unittest.main()
