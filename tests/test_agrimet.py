db308d7d830e36ceae015e09c1fb9b1cc7da5baf
dgketchum <dgketchum@gmail.com>
2017-10-02 13:39:06 -0600

    find closest station usin Vincenty formula

--- tests/test_agrimet.py
+++ tests/test_agrimet.py
@@ -15,18 +15,45 @@
 # ===============================================================================
 
 import unittest
+import json
+import requests
+from fiona import open as fopen
+
+from metio.agrimet import haversine, load_stations, find_closest_station
 
 
-class MyTestCase(unittest.TestCase):
+class TestAgrimet(unittest.TestCase):
     def setUp(self):
-        pass
+        self.point_file = 'tests/data/agrimet_test/points/agrimet_test.shp'
+        self.station_info = 'https://www.usbr.gov/pn/agrimet/agrimetmap/usbr_map.json'
 
-    def tearDown(self):
-        pass
+        self.site_ids = ['umhm', 'robi', 'hntu', 'faln', 'mdxo', 'masw']
 
-    def test_something(self):
-        self.assertEqual(True, False)
+    def test_loat_station_data(self):
+        r = requests.get(self.station_info)
+        stations = json.loads(r.text)
+        self.assertIsInstance(stations, dict)
 
+    def test_great_circle_dist(self):
+        # two eastern-most sites in MT
+        lat1, lon1 = 46.778, -105.298
+        lat2, lon2 = 46.988, -104.803
+        # dist = great_circle_distance(lat1, lon1, lat2, lon2)
+        dist = haversine(lat1, lon1, lat2, lon2)
+        self.assertAlmostEqual(60., dist, delta=5)
+
+    def test_find_closest_station(self):
+
+        data = load_stations()
+
+        coords = []
+        with fopen(self.point_file, 'r') as src:
+            for feature in src:
+                coords.append(feature['geometry']['coordinates'])
+
+        for coord in coords:
+            stn = find_closest_station(data, target_lon=coord[0], target_lat=coord[1])
+            self.assertTrue(stn in self.site_ids)
 
 if __name__ == '__main__':
     unittest.main()
