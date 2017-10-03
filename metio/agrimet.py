3c2dbe4fdd60ee443938db10de1f37ae0ae6d38f
dgketchum <dgketchum@gmail.com>
2017-10-03 11:15:52 -0600

    passing tests for pandas agrimet dataframe

--- metio/agrimet.py
+++ metio/agrimet.py
@@ -21,11 +21,32 @@ from datetime import datetime
 from fiona import collection
 from fiona.crs import from_epsg
 from geopy.distance import vincenty
-from climata.hydromet import AgrimetRecentIO
+from pandas import read_table, to_datetime
 
 STATION_INFO_URL = 'https://www.usbr.gov/pn/agrimet/agrimetmap/usbr_map.json'
+AGRIMET_REQ_SCRIPT = 'https://www.usbr.gov/pn-bin/agrimet.pl'
 # in km
 EARTH_RADIUS = 6371.
+
+WEATHER_PARAMETRS = [('DATETIME', 'Date - [YYYY-MM-DD]'),
+                     ('ET', 'Evapotranspiration Kimberly-Penman - [in]'),
+                     ('ETos', 'Evapotranspiration ASCE-EWRI Grass - [in]'),
+                     ('ETrs', 'Evapotranspiration ASCE-EWRI Alfalfa - [in]'),
+                     ('MM', 'Mean Daily Air Temperature - [F]'),
+                     ('MN', 'Minimum Daily Air Temperature - [F]'),
+                     ('MX', 'Maximum Daily Air Temperature - [F]'),
+                     ('PC', 'Accumulated Precipitation Since Recharge/Reset - [in]'),
+                     ('PP', 'Daily (24 hour) Precipitation - [in]'),
+                     ('PU', 'Accumulated Water Year Precipitation - [in]'),
+                     ('SR', 'Daily Global Solar Radiation - [langleys]'),
+                     ('TA', 'Mean Daily Humidity - [%]'),
+                     ('TG', 'Growing Degree Days - [base 50F]'),
+                     ('YM', 'Mean Daily Dewpoint Temperature - [F]'),
+                     ('UA', 'Daily Average Wind Speed - [mph]'),
+                     ('UD', 'Daily Average Wind Direction - [deg az]'),
+                     ('WG', 'Daily Peak Wind Gust - [mph]'),
+                     ('WR', 'Daily Wind Run - [miles]'),
+                     ]
 
 
 class Agrimet(object):
@@ -33,6 +54,8 @@ class Agrimet(object):
                  interval=None, lat=None, lon=None, sat_image=None):
 
         self.station_info_url = STATION_INFO_URL
+        self.station = station
+
         if not station:
             if not lat and not sat_image:
                 raise ValueError('Must initialize agrimet with a station, '
@@ -52,15 +75,15 @@ class Agrimet(object):
 
             self.today = datetime.now()
 
-            self.back_daily = (self.today - self.start).days
+            self.start_index = (self.today - self.start).days - 1
 
     @property
     def params(self):
         return urlencode(OrderedDict([
             ('cbtt', self.station),
             ('interval', self.interval),
-            ('format', 2),
-            ('back', self.back_daily)
+            ('format', 1),
+            ('back', self.start_index)
         ]))
 
     def find_closest_station(self, target_lat, target_lon):
@@ -86,8 +109,65 @@ class Agrimet(object):
         stations = json.loads(r.text)
         return stations
 
-    def fetch_data(self):
+    def fetch_data(self, return_raw=False):
+        url = '{}?{}'.format(AGRIMET_REQ_SCRIPT, self.params)
+        raw_data = read_table(url, skip_blank_lines=True,
+                              header=1, sep=r'\,|\t', engine='python',
+                              )
+        if return_raw:
+            return raw_data
 
+        reformed_data = self._reformat_dataframe(raw_data)
+
+        reformed_data.to_csv(path_or_buf='/data01/images/sandbox/drlm.csv')
+
+        return reformed_data
+
+    def _reformat_dataframe(self, df):
+
+        old_cols = df.columns.values.tolist()
+        new_cols = WEATHER_PARAMETRS
+        head_1 = []
+        head_2 = []
+        head_3 = []
+        for x in old_cols:
+            end = x.replace('{}_'.format(self.station), '')
+            for y, z in new_cols:
+                if end.upper() == y.upper():
+                    head_1.append(y.upper())
+                    desc, unit = z.split(' - ')
+                    head_2.append(desc)
+                    head_3.append(unit)
+                    break
+        df.columns = [head_1, head_2, head_3]
+        df.index = to_datetime(df.index)
+
+        # convert to standard units
+        for col in ['ET', 'ETRS', 'ETOS', 'PC', 'PP', 'PU']:
+            # in to mm
+            try:
+                df[col] *= 25.4
+            except KeyError:
+                pass
+        for col in ['MN', 'MX', 'MM', 'YM']:
+            # F to C
+            df[col] *= 9 / 5
+            df[col] += 32
+        for col in ['UA', 'WG']:
+            # mph to m s-1
+            df[col] *= 0.44704
+        # mi to m
+        df['WR'] *= 1609.34
+        # Langleys to J m-2
+        df['SR'] *= 41868.
+
+        headed_converted = ['[YYYY-MM-DD]', '[mm]', '[mm]', '[mm]',
+                            '[C]', '[C]', '[C]', '[mm]', '[mm]', '[W m-2]',
+                            '[%]', '[base 50F]', '[m sec-1]', '[deg az]',
+                            '[m sec-1]', '[m]', '[C]']
+        df.columns = [head_1, head_2, headed_converted]
+
+        return df
 
     @staticmethod
     def write_shp(json_data, epsg, out):
