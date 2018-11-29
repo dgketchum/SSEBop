a425901148d5b3b4b9da44b3576d5c28a2763633
dgketchum <dgketchum@gmail.com>
2018-11-29 16:00:50 -0700

    add file list

--- ssebop/interpolate.py
+++ ssebop/interpolate.py
@@ -13,15 +13,32 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ===============================================================================
+import os
+from pandas import read_csv, date_range, Series
 
-from pandas import read_csv, date_range
+
+DROP = ['SPACECRAFT_ID', 'SENSOR_ID', 'COLLECTION_NUMBER',
+        'COLLECTION_CATEGORY', 'SENSING_TIME', 'DATA_TYPE',
+        'WRS_PATH', 'WRS_ROW', 'CLOUD_COVER', 'NORTH_LAT',
+        'SOUTH_LAT', 'WEST_LON', 'EAST_LON', 'TOTAL_SIZE',
+        'BASE_URL']
+
+NOT_DROP = ['SCENE_ID', 'PRODUCT_ID']
 
 
-def reform_images_table(csv):
+def reform_images_table(csv, daily_data):
     csv = read_csv(csv, index_col=5, parse_dates=[5], header=0)
     odx = csv.index
     ndx = date_range(odx.min(), odx.max(), freq='D')
-    df = csv.reindex(odx.union(ndx)).interpolate('index').reindex(ndx)
+    df = csv.reindex(odx.union(ndx)).reindex(ndx).drop(columns=DROP)
+    _files = [(os.path.join(daily_data, x), x.replace('.tif', '')[-10:]) for x in os.listdir(daily_data)]
+    _files.sort(key=lambda x: x[1])
+    series = Series()
+
+    pass
+
+
+def interpolate():
     pass
 
 
